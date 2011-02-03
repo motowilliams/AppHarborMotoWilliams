3248167798aa943cfb21ddfd1a0ddac0bc3f88ef
motowilliams <EricWilliams76@gmail.com>
2011-02-02 22:35:58 -0700

    added sample twilio endpoint

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using AppHarborMotoWilliams.Models;
 using AppHarborMotoWilliams.Services;
 
@@ -28,5 +30,36 @@ namespace AppHarborMotoWilliams.Controllers
 		{
 			return View();
 		}
+
+		public JsonResult Hello()
+		{
+			var foo = new []{ 
+			new { Name = "Dakota", Age = 5 },
+			new { Name = "Logan", Age = 3 },
+			new { Name = "Shiloh", Age = 1 }
+			};
+			return Json(foo,JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult HandleCall(string To, string From, string CallSid)
+		{
+			var doc = new XDocument();
+			var response = new XElement("Response");
+
+			response.Add(new XElement("Say", "Thank you for calling, please leave a message."));
+			response.Add(new XElement("Record",
+							new XAttribute("maxLength", 30),
+							new XAttribute("action", Url.Action("HandleRecording"))
+						));
+
+			doc.Add(response);
+
+			return new ContentResult
+			{
+				ContentType = "text/xml",
+				Content = doc.ToString()
+			};
+		}
+
 	}
 }
